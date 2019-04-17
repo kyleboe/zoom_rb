db90e939b5d9c230447379fc5fc95e096eee7fa2
shohey1226 <shoheik@cpan.org>
2019-04-17 04:10:05 +0000

    added delete file

--- lib/zoom/actions/recording.rb
+++ lib/zoom/actions/recording.rb
@@ -28,6 +28,14 @@ module Zoom
         Utils.require_params([:meeting_id], options)
         Utils.parse_response self.class.delete("/meetings/#{options[:meeting_id]}/recordings",  query: options.except(:meeting_id), headers: request_headers)
       end
+
+      def recording_delete_file(*args)
+        options = Utils.extract_options!(args)
+        Utils.require_params([:meeting_id], options)
+        Utils.require_params([:recording_id], options)
+        Utils.parse_response self.class.delete("/meetings/#{options[:meeting_id]}/recordings/#{options[:recording_id]}",  query: options.except(:meeting_id).excpet(:recording_id), headers: request_headers)
+      end
+
     end
   end
 end
