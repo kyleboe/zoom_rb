3bec4db4802dea03b9480bccc82c3059a7bcdd3b
Scott Andersen <scott@procpr.org>
2020-01-06 09:44:21 -0500

    Date/time not supported after all

--- lib/zoom/actions/phone.rb
+++ lib/zoom/actions/phone.rb
@@ -4,10 +4,7 @@ module Zoom
   module Actions
     module Phone
       def phone_recording_list(*args)
-        options = Utils.extract_options!(args)
-        Utils.process_datetime_params!(%i[from to], options)
-
-        params = Zoom::Params.new(options)
+        params = Zoom::Params.new(Utils.extract_options!(args))
         params.require(:id)
         Utils.parse_response self.class.get("/phone/users/#{params[:id]}/recordings", query: params.except(:id), headers: request_headers)
       end
