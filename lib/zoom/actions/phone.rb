e5a4269c1b3be7b47adbac4bc0b60b3c80d8832c
Scott Andersen <scott@procpr.org>
2020-01-06 10:20:55 -0500

    Method for phone user list

--- lib/zoom/actions/phone.rb
+++ lib/zoom/actions/phone.rb
@@ -8,6 +8,13 @@ module Zoom
         params.require(:id)
         Utils.parse_response self.class.get("/phone/users/#{params[:id]}/recordings", query: params.except(:id), headers: request_headers)
       end
+
+      def phone_user_list(*args)
+        params = Zoom::Params.new(Utils.extract_options!(args))
+        params.permit(%i[page_size page_number])
+        response = self.class.get('/phone/users', query: params, headers: request_headers)
+        Utils.parse_response(response)
+      end
     end
   end
 end
