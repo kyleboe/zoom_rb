4dd3ca726dc577f27c5dd3e430736475ed56f8ca
Scott Andersen <scott@procpr.org>
2020-01-06 09:24:41 -0500

    Add to/from params

--- lib/zoom/actions/phone.rb
+++ lib/zoom/actions/phone.rb
@@ -6,6 +6,7 @@ module Zoom
       def phone_recording_list(*args)
         params = Zoom::Params.new(Utils.extract_options!(args))
         params.require(:id)
+        Utils.process_datetime_params!(%i[from to], options)
         Utils.parse_response self.class.get("/phone/users/#{params[:id]}/recordings", query: params.except(:id), headers: request_headers)
       end
     end
