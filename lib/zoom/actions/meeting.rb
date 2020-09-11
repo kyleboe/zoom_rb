faa1e6cfcd57a80060b786d46dc52a3974cea57c
Alex Pita <alex.pita1973@gmail.com>
2020-09-11 17:58:27 -0400

    CAL-30: necessary for supporting recurring appointments telehealth sessions

--- lib/zoom/actions/meeting.rb
+++ lib/zoom/actions/meeting.rb
@@ -29,7 +29,7 @@ module Zoom
       def meeting_get(*args)
         options = Zoom::Params.new(Utils.extract_options!(args))
         options.require(%i[meeting_id])
-        Utils.parse_response self.class.get("/meetings/#{options[:meeting_id]}", headers: request_headers)
+        Utils.parse_response self.class.get("/meetings/#{options[:meeting_id]}", query: options.except(:meeting_id), headers: request_headers)
       end
 
       # Update meeting info on Zoom via meeting ID.
