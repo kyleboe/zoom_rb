390530ccaa08e20914228d1ae85c6ac4dcdba0d5
Scott Andersen <scott@scottxp.com>
2022-10-31 09:57:54 -0400

    Add group list method to api

--- lib/zoom/actions/group.rb
+++ lib/zoom/actions/group.rb
@@ -3,6 +3,12 @@
 module Zoom
   module Actions
     module Group
+      def group_list(*args)
+        params = Zoom::Params.new(Utils.extract_options!(args))
+        params.permit(%i[page_size page_number])
+        response = self.class.get('/groups/#{params[:id]}/members', query: params, headers: request_headers)
+        Utils.parse_response(response)
+      end
     end
   end
 end
