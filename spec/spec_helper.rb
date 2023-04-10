b0216391aee8e32a8d9916f08e6e3808556336bc
Kyle Boe <kyle@boe.codes>
2023-04-10 12:44:40 -0700

    Ditch rails config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,17 +2,6 @@
 
 require 'simplecov'
 SimpleCov.start do
-  if ENV['CI']
-    require 'simplecov-lcov'
-
-    SimpleCov::Formatter::LcovFormatter.config do |c|
-      c.report_with_single_file = true
-      c.single_report_path = 'coverage/lcov.info'
-    end
-
-    formatter SimpleCov::Formatter::LcovFormatter
-  end
-
   add_filter %w[version.rb]
 end
 
